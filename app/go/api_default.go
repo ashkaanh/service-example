27c87b61a2aceca905914453bb8fe103657d36fc
Ashkan <ashkan.link@gmail.com>
2023-02-01 09:43:28 +0000

    Add application and Helm chart

---
+++ app/go/api_default.go
@@ -0,0 +1,10 @@
+package swagger
+
+import (
+	"net/http"
+)
+
+func RootGet(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+	w.WriteHeader(http.StatusOK)
+}
